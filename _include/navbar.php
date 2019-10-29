c56172e78b08f7a75e62d95c31cc1e7412ae689d
fathur rohman <rahmanboy987@gmail.com>
2019-10-29 21:38:29 +0700

    final malam ini

---
+++ _include/navbar.php
@@ -0,0 +1,59 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#" style="text-align:center">LOGO DISINI<br> FORMATIK</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            <a href="" style="color:white">LOGIN</a>
+            <p style="color:white; margin:10px;">|</p>
+            <a href="" style="color:white">REGISTER</a>
+        </form>
+    </div>
+</nav>
+
+<div class="navbar-light bg-light">
+    <div class="container">
+        <div class="row" style="font-size:20px;">
+            <div class="col" style="">
+                <a href="">SKRIPSI</a>
+                <a href="">KKN</a>
+                <a href="">PKL</a>
+                <a href="">SHITPOST</a>
+                <a href="">TUTORIAL</a>
+                <a href="">KARYA</a>
+            </div>
+            <div class="col-3" style="">
+                <a href=""><i class="fas fa-bell"></i></a>
+                <a href=""><i class="fas fa-comment-dots"></i></a>
+                <a href=""><i class="fas fa-cog"></i></a>
+                <a href="">username anda</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ js/include_js.php
@@ -0,0 +1,2 @@
+<script src="<?= base_url() ?>/css/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>/css/script.js"></script>
