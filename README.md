a10ce016b6b5bde30a6a868b3ca389bf5305e402
fathur rohman <rahmanboy987@gmail.com>
2019-10-29 20:38:08 +0700

    by fathur

--- README.md
+++
@@ -1,6 +0,0 @@
-# FP-Pemweb
-Website ini dibangun untuk memenuhi Final Project mata kuliah Pemrograman Web
-
-nb : untuk memudahkan, tolong setiap commit dikasih tulisan "update by [namamu]" biar gk bingung
-
-untuk memudahkan nanti tampilan onlinenya insyaallah akan saya porting ke website http://fp.gabutin.site
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Kelompok 3">
+    <title>FORMATIK - Main</title>
+</head>
+
+<body>
+    <?php include "_include/header.php" ?>
+    <?php include "_include/sidebar.php" ?>
+
+
+
+    <?php include "_include/footer.php" ?>
+</body>
+
+</html>
