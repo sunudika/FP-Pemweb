ce1099ed6544a75e6381eb19ff25bddd9c309a9d
Sunu Ilham Pradika <sunudika@gmail.com>
2019-11-30 15:22:05 +0700

    fixing login.php

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -8,15 +11,16 @@
 <body>
     <div class="login-card"><img src="aset/logo2.png" class="profile-img-card" />
         <p class="profile-name-card"> </p>
-        <form class="form-signin"><span class="reauth-email"> </span>
-            <input class="form-control" type="email" required placeholder="Email address" autofocus id="inputEmail" />
-            <input class="form-control" type="password" required placeholder="Password" id="inputPassword" />
+        <form class="form-signin" method="post" action="login.php">
+            <input class="form-control" type="text" name="username" required placeholder="Username" autofocus id="inputEmail" />
+            <input class="form-control" type="password" name="password" required placeholder="Password" id="inputPassword" />
             <div class="checkbox">
                 <div class="checkbox">
                     <label>
                         <input type="checkbox" />Remember me</label>
                 </div>
             </div>
-            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
+            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit" name="login_user">Sign in</button>
         </form><a href="#" class="forgot-password">Forgot your password?</a></div>
 </body>
+</html>
