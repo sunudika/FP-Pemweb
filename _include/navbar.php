988aee3b44a4920538f3a472bd28f90463601dc2
Cherrydet <cherrydaniella10@gmail.com>
2019-12-07 09:03:34 +0700

    ngubah tampilan je

--- _include/navbar.php
+++ _include/navbar.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="<?= base_url() ?>" style="text-align:center"><img src="<?= base_url() ?>/images/aset/logo3.png" alt="LOGO FORMATIK" height="50"></a>
+    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,15 +10,18 @@
         <form class="form-inline my-2 my-lg-0" method="get" action="search.php">
             <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            
             <?php if (!isset($_SESSION['username'])) { ?>
-                <a href="login.php" style="color:white">LOGIN</a>
+                <a href="login.php" style="color:white; margin-left: 10px;">LOGIN</a>
                 <p style="color:white; margin:10px;">|</p>
                 <a href="register.php" style="color:white">REGISTER</a> &nbsp;
             <?php } else { ?>
                 <!-- logged in user information -->
-                <a href="_auth/logout.php" style="color: red; padding: 10px;padding-right:150px; font-weight:bold; font-size:20px;">logout</a>
+                <a href="index.php?logout='1'" style="color: red; padding: 10px;padding-right:150px; font-weight:bold; font-size:20px;">logout</a>
             <?php } ?>
         </form>
+
+
         <form class="form-inline my-2 my-lg-0"> </form>
     </div>
 </nav>
--- register.php
+++ register.php
@@ -1,145 +1,60 @@
-<?php include "config.php";
+<?php include "config.php"; ?>
 
-if ($toogle_maintainance == 1) {
-	echo "<script>window.location='" . base_url() . "/maintenis.php';</script>";
-} else { ?>
+<!DOCTYPE html>
+<html>
 
-	<!DOCTYPE html>
-	<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/logreg.css" />
+	<title>FORMATIK - Registration</title>
+	<link rel="icon" href="./images/aset/logo.png" type="image/x-icon">
 
-	<head>
-		<title>Registration system PHP and MySQL</title>
-	</head>
+</head>
 
-	<style>
-		* {
-			margin: 0px;
-			padding: 0px;
-		}
+<body>
+	<div class="reg-card"><img src="images/aset/logo3.png" >
 
-		body {
-			font-size: 120%;
-			background: #F8F8FF;
-		}
+	<form method="post" action="register.php" enctype="multipart/form-data">
+		<?php
+		if (count($errors) > 0) { ?>
+			<div class="error">
+				<?php foreach ($errors as $error) { ?>
+					<p><?php echo $error ?></p>
+				<?php } ?>
+			</div>
+		<?php } ?>
 
-		#img-div {
-			width: 50px;
-			height: 50px;
-		}
-
-		.header {
-			width: 30%;
-			margin: 50px auto 0px;
-			color: white;
-			background: #5F9EA0;
-			text-align: center;
-			border: 1px solid #B0C4DE;
-			border-bottom: none;
-			border-radius: 10px 10px 0px 0px;
-			padding: 20px;
-		}
-
-		form,
-		.content {
-			width: 30%;
-			margin: 0px auto;
-			padding: 20px;
-			border: 1px solid #B0C4DE;
-			background: white;
-			border-radius: 0px 0px 10px 10px;
-		}
-
-		.input-group {
-			margin: 10px 0px 10px 0px;
-		}
-
-		.input-group label {
-			display: block;
-			text-align: left;
-			margin: 3px;
-		}
-
-		.input-group input {
-			height: 30px;
-			width: 93%;
-			padding: 5px 10px;
-			font-size: 16px;
-			border-radius: 5px;
-			border: 1px solid gray;
-		}
-
-		.btn {
-			padding: 10px;
-			font-size: 15px;
-			color: white;
-			background: #5F9EA0;
-			border: none;
-			border-radius: 5px;
-		}
-
-		.error {
-			width: 92%;
-			margin: 0px auto;
-			padding: 10px;
-			border: 1px solid #a94442;
-			color: #a94442;
-			background: #f2dede;
-			border-radius: 5px;
-			text-align: left;
-		}
-
-		.success {
-			color: #3c763d;
-			background: #dff0d8;
-			border: 1px solid #3c763d;
-			margin-bottom: 20px;
-		}
-	</style>
-
-	<body>
-		<div class="header">
-			<h2>Register</h2>
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			Foto KTM: <input type="file" name="image"><br>
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_user">Register</button>
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn-sign" name="sign-in">Sign In</button>
 		</div>
 
-		<form method="post" action="register.php" enctype="multipart/form-data">
-			<?php
-				if (count($errors) > 0) { ?>
-				<div class="error">
-					<?php foreach ($errors as $error) { ?>
-						<p><?php echo $error ?></p>
-					<?php } ?>
-				</div>
-			<?php } ?>
 
-			<div class="input-group">
-				<label>Username</label>
-				<input type="text" name="username">
-			</div>
-			<div class="input-group">
-				<label>Email</label>
-				<input type="email" name="email">
-			</div>
-			<div class="input-group">
-				<label>Password</label>
-				<input type="password" name="password_1">
-			</div>
-			<div class="input-group">
-				<label>Confirm password</label>
-				<input type="password" name="password_2">
-			</div>
-			<div class="input-group">
-				Foto KTM: <input type="file" name="image"><br>
-			</div>
-			<div class="input-group">
-				<button type="submit" class="btn" name="reg_user">Register</button>
-			</div>
+	</form>
+	</div>
+</body>
 
-			<p>
-				Already a member? <a href="login.php">Sign in</a>
-			</p>
-			<p>Gak mau DAFTAR? Jadi tamu aja lur! <a href="index.php">Home</a> </p>
-		</form>
-	</body>
-
-	</html>
-<?php } ?>
+</html>
