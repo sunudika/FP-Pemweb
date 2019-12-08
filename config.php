f7bd4f5bcdc78b335ceb8c39846c064226e31a9c
fathur rohman <rahmanboy987@gmail.com>
2019-12-08 22:19:09 +0700

    update

--- config.php
+++ config.php
@@ -43,18 +43,17 @@ if (isset($_POST['reg_user'])) {
     $user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($con, $user_check_query);
     $user = mysqli_fetch_assoc($result);
-    
+
     if ($user) {
         if ($user['username'] === $username) {
-        array_push($errors, "Username sudah digunakan, cari yg lain");
+            array_push($errors, "Username sudah digunakan, cari yg lain");
         }
 
         if ($user['email'] === $email) {
-        array_push($errors, "email sudah digunakan, cari yg lain");
+            array_push($errors, "email sudah digunakan, cari yg lain");
         }
     }
 
-    $extphotovalid = ['jpg', 'jpeg', 'png'];
     $extphoto = explode('.', $photo);
     $extphoto = strtolower(end($extphoto));
 
@@ -64,9 +63,8 @@ if (isset($_POST['reg_user'])) {
 
     if (count($errors) == 0) {
         $password = md5($password_1);
-  
-        $query = "INSERT INTO user (username, email, password, img_verification) 
-                  VALUES('$username', '$email', '$password', '$image')";
+
+        $query = "INSERT INTO user (username, email, password, img_verification) VALUES('$username', '$email', '$password', '$photo')";
         mysqli_query($con, $query);
         echo "<script>alert('Pendaftaran Berhasil')</script>";
         header('location: index.php');
