ae282a2f8dc48f7923bfcd40cc9b7820a83d35ad
Sunu Ilham Pradika <sunudika@gmail.com>
2019-12-08 23:39:34 +0700

    perbaikan chat

--- _include/content_chat.php
+++ _include/content_chat.php
@@ -1,4 +1,4 @@
-<button class="open-button" onclick="openForm()">Chat</button>
+<button class="open-button" id="open-chat" onclick="openForm()">Chat</button>
 <div class="chat-popup" id="myForm">
     <div id="content" style="margin-top:10px;height:100%;">
         <div class="chat">
@@ -17,5 +17,5 @@
             </div>
         </div>
     </div>
-    <button type="button" class="btn cancel" onclick="closeForm()" style="border-radius: 3px; background-color: #a41623; color:white;">Close</button>
+    <button type="button" id="chat-close" class="btn cancel" onclick="closeForm()" style="border-radius: 3px; background-color: #a41623; color:white;">Close</button>
 </div>
--- index.php
+++ index.php
@@ -53,6 +53,18 @@ if ($toogle_maintainance == 1) {
         function closeForm() {
             document.getElementById("myForm").style.display = "none";
         }
+
+        $(function() {
+		$("#open-chat").click(function(){
+		$("#open-chat").hide();
+            });
+	    });
+
+        $(function() {
+		$("#chat-close").click(function(){
+		$("#open-chat").show();
+            });
+	    });
     </script>
 
 <?php } ?>
