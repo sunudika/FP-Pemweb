0275aec8cbba2fd8b58d66094fa75e004ad4957c
Sunu Ilham Pradika <sunudika@gmail.com>
2019-12-09 08:32:39 +0700

    prepare for notification feature

--- css/include_css.php
+++ css/include_css.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?= base_url() ?>/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?= base_url() ?>/css/style.css">
 <link rel="stylesheet" href="<?= base_url() ?>/css/chat.css">
+<link rel="stylesheet" href="<?= base_url() ?>/css/notification.css">
 <script src="https://kit.fontawesome.com/659604d02a.js" crossorigin="anonymous"></script>
