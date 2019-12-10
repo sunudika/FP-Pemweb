a0bf075f8e271cf46f7611bbaeed4b35aa04939d
fathur rohman <rahmanboy987@gmail.com>
2019-12-10 12:03:44 +0700

    Update content_post.php

--- _include/content_post.php
+++ _include/content_post.php
@@ -132,7 +132,7 @@
 
                 <div style="background-color:rgba(255, 255, 255, 0.25); padding: 5px 0;">
                     <?php
-                            $sql_comment = mysqli_query($con, "SELECT * FROM comment JOIN user on comment.nama_user=user.username WHERE id_post='$post_id' ORDER BY id") or die(mysqli_error($con, ""));
+                            $sql_comment = mysqli_query($con, "SELECT * FROM comment JOIN user on comment.nama_user=user.username WHERE id_post='$post_id' ORDER BY comment.id") or die(mysqli_error($con, ""));
                             if (mysqli_num_rows($sql_comment) > 0) {
                                 while ($comment = mysqli_fetch_array($sql_comment)) { ?>
                             <div style="padding: 0 30px">
